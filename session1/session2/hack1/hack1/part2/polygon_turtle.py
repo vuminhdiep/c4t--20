f6ad571b9aa63f5a6d9872cb7951eaee7da24dfb
vuminhdiep <vuminhdiep.hycmd@gmail.com>
2019-06-14 20:39:52 +0700

    Sua loi bai polygon turtle cua bai minihack1

--- session1/session2/hack1/hack1/part2/polygon_turtle.py
+++ session1/session2/hack1/hack1/part2/polygon_turtle.py
@@ -1,8 +1,9 @@
 
+n = int(input("Nhap so canh vao day"))
+print(n)
+a = (n-2)*180/n
 from turtle import *
 shape("turtle")
-n = int(input("Nhap so canh vao day")
-a = int((n-2)*180/n)
 for i in range(n):
     forward(100)
     right(a)
